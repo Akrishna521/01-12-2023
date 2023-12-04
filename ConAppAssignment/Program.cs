752b0aaccece2c4b0834e184b2536f73ee739259
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-12-04 15:19:22 +0530

    01-12-2023

---
+++ ConAppAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MySharedLibrary
+{
+    public class MyUtilityClass
+    {
+        public void ValidateData(string data)
+        {
+            Console.WriteLine($"Validating data: {data}");
+        }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"Logging message: {message}");
+        }
+
+        public string EncryptData(string data)
+        {
+            return $"Encrypted: {data}";
+        }
+    }
+}
